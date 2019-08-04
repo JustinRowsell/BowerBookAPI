bd2a188b1727b1a0940bf8db0af9114343ffad5d
JustinRowsell <Rowsell64@gmail.com>
2019-08-04 16:43:34 -0600

    fixed deployment config for 2.2

--- src/BowerBookAPI/LocalEntryPoint.cs
+++ src/BowerBookAPI/LocalEntryPoint.cs
@@ -1,21 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.Json;
+using BowerBookAPI;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Threading.Tasks;
 
-namespace BowerBookAPI
+namespace AWSServerless1
 {
     /// <summary>
     /// The Main function can be used to run the ASP.NET Core application locally using the Kestrel webserver.
+    /// It is now also the main entry point for the custom runtime.
     /// </summary>
     public class LocalEntryPoint
     {
-        public static void Main(string[] args)
+        private static readonly LambdaEntryPoint LambdaEntryPoint = new LambdaEntryPoint();
+        private static readonly Func<APIGatewayProxyRequest, ILambdaContext, Task<APIGatewayProxyResponse>> Func = LambdaEntryPoint.FunctionHandlerAsync;
+
+        public static async Task Main(string[] args)
         {
+#if DEBUG
             BuildWebHost(args).Run();
+#else
+            // Wrap the FunctionHandler method in a form that LambdaBootstrap can work with.
+            using (var handlerWrapper = HandlerWrapper.GetHandlerWrapper(Func, new JsonSerializer()))
+
+            // Instantiate a LambdaBootstrap and run it.
+            // It will wait for invocations from AWS Lambda and call the handler function for each one.
+            using (var bootstrap = new LambdaBootstrap(handlerWrapper))
+            {
+                await bootstrap.RunAsync();
+            }
+#endif
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
